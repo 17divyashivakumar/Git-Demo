1be0c01f0425f7f767b7d0dba07b021c26231299
17divyashivakumar <divyashivakumar334@gmail.com>
2025-06-18 10:35:39 +0530

    staging

--- demo.js
+++ demo.js
@@ -1,3 +1,11 @@
 console.log("Hello and Welcome to the Git Hub World!!")
 
 console.log("Hello Beautiful welcome to this universe")
+
+//selecting the files and add to the online - staging the changes
+
+//to add the changes click the + one, if you want to remove the changes then click the -
+
+//If you want to discard the changes then click on the discard change
+
+console.log("What to do")
