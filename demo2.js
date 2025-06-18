a7f997f4565a161f2ca3af3c794c3048088e9f1a
17divyashivakumar <divyashivakumar334@gmail.com>
2025-06-18 10:31:53 +0530

    change

--- demo2.js
+++ demo2.js
@@ -2,4 +2,3 @@ console.log("Hello Beautiful welcome to this universe")
 
 console.log("Hello and Welcome to the Git Hub World!!")
 //in the three dots select pull,push and then select the push
-console.log("hey")
